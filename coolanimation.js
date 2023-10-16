a3cfc6a8ec8b7b382bead5c5068406d244129bfc
Isaiah098 <147713457+Isaiah098@users.noreply.github.com>
2023-10-16 16:27:35 +0800

    Add files via upload

---
+++ coolanimation.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll(".hidden");
+  hiddenElements.forEach((el) => observer.observe(el));
+
+  const buyButton = document.getElementById("buyButton");
+  buyButton.addEventListener("click", function () {
+    alert("This is a dummy button miss :)");
+  });
+});
